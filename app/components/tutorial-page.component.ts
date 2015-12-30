0dbfc00208d8d3ed71738b32791ed4bd2df29c8e
Georg Schmidl <georg.schmidl@gmail.com>
2015-12-31 00:20:59 +0100

    Add field to tutorial page navigation

--- app/components/tutorial-page.component.ts
+++ app/components/tutorial-page.component.ts
@@ -1,14 +1,16 @@
 import {Component, OnInit} from "angular2/core";
 import {RouteParams} from "angular2/router";
 import {TutorialService} from "../services/tutorial.service";
+import {FieldService} from "../services/field.service";
 import {Tutorial} from "../models/tutorial";
+import {Field} from "../models/field";
 import {NavigationComponent} from "./navigation.component";
 import {TutorialDetailComponent} from "./tutorial-detail.component";
 
 @Component({
     selector: "tutorial",
     template: `
-      <navigation></navigation>
+      <navigation [field]="field"></navigation>
       <div class="container-fluid">
         <div class="row">
           <div class="col-md-8">
@@ -17,20 +19,24 @@ import {TutorialDetailComponent} from "./tutorial-detail.component";
         </div>
       </div>`,
     directives: [NavigationComponent, TutorialDetailComponent],
-    providers: [TutorialService]
+    providers: [TutorialService, FieldService]
 })
 
 export class TutorialPageComponent implements OnInit {
   public tutorial: Tutorial;
+  public field: Field;
 
   constructor(private _tutorialService: TutorialService,
+    private _fieldService: FieldService,
     private _routeParams: RouteParams) {
   }
 
   ngOnInit() {
     if (!this.tutorial) {
       let id = +this._routeParams.get("id");
-      this._tutorialService.getTutorial(id).then(tutorial => this.tutorial = tutorial);
+      this._tutorialService.getTutorial(id)
+      .then(tutorial => this.tutorial = tutorial)
+      .then(tutorial => this._fieldService.getField(tutorial.id).then(field => this.field = field));
     }
   }
 }
