20475dbec8ac3aebdf9cb1dd9d67586496cbdcea
Georg Schmidl <georg.schmidl@gmail.com>
2015-12-31 00:25:26 +0100

    Remove bottom margin from alert banner

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,6 +6,7 @@ import {TutorialPageComponent} from "./components/tutorial-page.component";
 
 @Component({
     selector: "app",
+    styles: [".alert {margin-bottom: 0}"],
     template: `
       <div class="alert alert-warning alert-dismissible" role="alert">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
