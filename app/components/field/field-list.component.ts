f7ba49b17c60586c765c52332cbeef4ddb6813f5
Georg Schmidl <georg.schmidl@gmail.com>
2016-02-14 20:46:28 +0100

    Add icons to fields

--- app/components/field/field-list.component.ts
+++ app/components/field/field-list.component.ts
@@ -5,12 +5,14 @@ import {Field} from "../../models/field";
 
 @Component({
     selector: "field-list",
-    styles: [".card {cursor: pointer}"],
+    styles: [".card {cursor: pointer}", "i.fa {font-size: 3em}"],
     template: `
       <div class="row">
         <div *ngFor="#field of fields" class="col-xs-12" [ngClass]="{'col-sm-6': fields.length > 1, 'col-md-4': fields.length > 2, 'col-lg-3': fields.length > 3, 'col-xl-2': fields.length > 5}">
           <div class="card text-xs-center card-buildingstuff-field" (click)="gotoField(field)">
             <div class="card-block">
+              <i *ngIf="field.icon" class="fa {{field.icon}}"></i>
+              <i *ngIf="!field.icon" class="fa fa-cog"></i>
               <h4 class="card-title">{{field.name}}</h4>
             </div>
           </div>
--- app/models/field.ts
+++ app/models/field.ts
@@ -2,4 +2,5 @@ export interface Field {
   id: number;
   name: string;
   itemsetIds: number[];
+  icon: string
 }
